16b04d0e9eede1b2de1cd9f999ac442a51592e1a
Lesly Anda <lesly.anda@cimat.mx>
2025-04-30 03:48:07 +0000

    EIIT9 almos finished

--- Tareas/Tarea9/T9IEDA_LeslyAnda.cpp
+++ Tareas/Tarea9/T9IEDA_LeslyAnda.cpp
@@ -1,19 +1,9 @@
 # include <bits/stdc++.h>
 
-# include <ext/pb_ds/assoc_container.hpp> // common file
-# include <ext/pb_ds/tree_policy.hpp> // including tree_order_statistics_node_update
-
 using namespace std;
-using namespace __gnu_pbds;
 
-typedef tree <pair<int, string>, // change type
-            null_type,
-            less< pair<int, string> >, // change type
-            rb_tree_tag,
-            tree_order_statistics_node_update> ordered_set;
-
-ordered_set uniqueSubStrings(const string& s){
-    const int p = 31; // 33
+int goodSubStrings(const string& s, const string& val, const int& k){
+    const int p = 33; // 33
     const int m = 1000000007;
 
     // precalculamos las potencias
@@ -22,26 +12,42 @@ ordered_set uniqueSubStrings(const string& s){
     for(int i = 0; i< s.size(); i++)
         pi[i] = (pi[i-1]*p) % m;
 
-    // obtenemos los hash vals de los prefijos
+    // obtenemos los hash y vals de los prefijos
     vector <long long> hashedPrefix(s.size()+1, 0);
     for(int i = 0; i < s.size(); i++)
         hashedPrefix[i+1] = (hashedPrefix[i] + (s[i] - 'a' + 1) * pi[i]) % m;
 
-    ordered_set disctint;
+    // obtenemos los vals de los prefijos
+    vector <int> goodPrefix(s.size(), 0);
+    // calculate the first value
+    if (val[s[0] -'a'] == '0'){
+        goodPrefix[0] = 1;
+    } else goodPrefix[0] = 0;
+    // get the prefix values
+    for(int i = 1; i < s.size(); i++){
+        goodPrefix[i] = goodPrefix[i-1];
+        if (val[s[i] -'a'] == '0'){
+            goodPrefix[i]++;
+        }
+    }
+    
+    set<long long> disctint;
     
     // cycle over substring lenghts
     for(int l = 1; l <= s.size(); l++){
-        // set <long long> allHashed;
         // cycle over string position
         for(int i = 0; i <= s.size() - l; i++){
             long long hashed = (hashedPrefix[i+l] + m - hashedPrefix[i]) % m; // +m?
             // to compare we multiply by p^{n-1-i}
             hashed = (hashed * pi[s.size()-i-1]) % m;
-            disctint.insert(make_pair(hashed, s.substr(i, l)));
+            if(goodPrefix[i+l-1]-goodPrefix[i-1] <= k){
+                // cout << s.substr(i, l) << "-" << goodPrefix[i+l-1]-goodPrefix[i-1]<< endl;
+                disctint.insert(hashed);
+            }
         }
     }
 
-    return disctint;
+    return disctint.size();
 }
 
 set<pair<string, int>> uniqueGoodSubStrings(const string& s, const string& val, const int& k){
@@ -95,21 +101,9 @@ int main(){
     string s, val; int k;
     cin >> s >> val >> k;
 
-    set<pair<string, int>> strArr = uniqueGoodSubStrings(s, val, k);
-    int result = strArr.size();
-    // for(const auto str : strArr) cout << str.first << " " << str.second << endl;
-    // for(const auto str : strArr){
-    //     int count = 0;
-    //     for(const auto c : str.second){
-    //         if (val[c -'a'] == '0'){
-    //             count ++;
-    //         }
-    //     }
-    //     if (count > k) result--;
-    // }
-    // for(const auto str : strArr) cout << str.first << " " << str.second << endl;
-    
-    cout << strArr.size() << endl;
+    int sol = goodSubStrings(s, val, k);
+
+    cout << sol << endl;
 
     return 0;
 }
--- Tareas/Tarea9/T9IIEDA_LeslyAnda.cpp
+++ Tareas/Tarea9/T9IIEDA_LeslyAnda.cpp
@@ -2,34 +2,102 @@
 
 using namespace std;
 
-unsigned long myhash(unsigned char *str) {
-    unsigned long hash = 5381;
-    int c;
-    while (c = *str++)
-    hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    return hash;
+class Password{
+    string s; int n;
+    vector<long long> hashed_prefixes;
+    vector<long long> pi;
+
+    const int p = 33;
+    const int m = 1000000007;
+
+void getPi(){
+    pi[0] = 1;
+    for (int i = 1; i < s.size(); i++)
+        pi[i] = (pi[i-1] * p) % m;
 }
 
-vector<vector<int>> clusterIdenticalStrings(const vector<string> &arr) {
-    vector<pair<long long, int>> hashValues(arr.size());
-    for (int i = 0; i < arr.size(); i++)
-        hashValues[i] = make_pair(static_cast<long long>(myhash(arr[i])), i);
-    // Sort the hash values
-    sort(hashValues.begin(), hashValues.end());
-    vector<vector<int>> clusters;
-    for (int i = 0; i < arr.size(); i++) {
-        // We create a new group
-        if (i == 0 || hashValues[i].first != hashValues[i-1].first)
-        clusters.push_back(vector<int>());
-        // We put the string index in the current group
-        clusters.back().push_back(hashValues[i].second);
+void getHashPfx() {
+    
+    for (int i = 0; i < s.size(); i++)
+        hashed_prefixes[i+1] = (hashed_prefixes[i] + (s[i] - 'a' + 1) * pi[i]) % m;
+
+    // map<string, long long> distinct;
+
+    // // Cycle over substring lengths
+    // for (int l = 1; l <= n; l++) {
+    //     // Cycle over starting positions
+    //     for (int i = 0; i <= n - l; i++) {
+    //         long long hashed = (hashed_prefixes[i + l] + m - hashed_prefixes[i]) % m; // hp[0] = 0 y hp[1] corresponde a s[0]
+    //         // To compare hashed values, set them all to power pˆ{n-1}
+    //         // As the difference above is the hash multiplies by pˆi
+    //         // we multiply by pˆ{n-1-i}
+    //         hashed = (hashed * pi[n-i-1]) % m;
+    //         distinct[s.substr(i, l)] = hashed;
+    //     }
+    // }
+}
+public:
+long long getHashVal(int i, int l){
+    // cout << s.substr(i,l);
+    long long hashed = (hashed_prefixes[i + l] + m - hashed_prefixes[i]) % m; // hp[0] = 0 y hp[1] corresponde a s[0]
+    hashed = (hashed * pi[n-i-1]) % m;
+    return hashed;
+}
+
+int BSTL(int L){ // binarySearchTheLenght
+    // si no funcionó para ninguna len
+    // cout << L << endl;
+    if(L == 0) return L; 
+
+    if(worksFor(L)){
+        // cout << "funcionó para " << L << endl;
+        if(!worksFor(L+1)) return L;
+        L = BSTL(L + L/2);
     }
-    return clusters;
+    else{
+        if(worksFor(L-1)) return L-1;
+        L = BSTL(L - L/2);
     }
+
+    return L;
+}
+
+bool worksFor(int L){
+    // cout << "comparando: " << s.substr(0,L) << " con " << s.substr(n-L, L) << endl;
+    long long hpfx = getHashVal(0, L), hsfx = getHashVal(n-L, L);
+    if(hpfx != hsfx) return false;
+
+    for(int i = 1; i < n-L; i++)
+        if(getHashVal(i, L) == hpfx) return true;
+
+    // cout << " returning false" << endl;
+    return false;
+}
+
+public:
+Password(const string &_s){
+    s = _s;
+    n = s.size();
+    hashed_prefixes.assign(s.size() + 1, 0);
+    pi.resize(s.size());
+
+    getPi();
+    getHashPfx();
+}
+
+void solve(){
+    int L = BSTL(2*s.size()/3); // iniciamos con el piso de 2*N/3;
+    if(L == 0) cout << "Just a legend" << endl;
+    else cout << s.substr(0, L) << endl;
+}
+
+};
 
 int main(){
-    vector<string> strarr = {"f", "fi", "fif", "fifi", "fifif", "fififi", "i", "if", "ifi", "ifif", "ififi", "f", "fi", "fif", "fifi", "i", "if", "ifi", "f", "fi", "i"};
-    vector<vector<int>> clusters = clusterIdenticalStrings(strarr);
+    string s; cin >> s;
+    Password pwd(s);
 
+    // cout << s.substr(0,3) <<":" << endl << pwd.getHashVal(0, 3) << endl << s.substr(s.size()-3, 3) << ":" << endl << pwd.getHashVal(14-3, 3) << endl;
+    pwd.solve();
     return 0;
 }
