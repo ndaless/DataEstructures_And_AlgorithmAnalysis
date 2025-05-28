a3abae39738fa0b6605a7dbc2b8ee220f3c51462
Lesly Anda <lesly.anda@cimat.mx>
2025-05-28 14:26:25 -0600

    Create DONE_11786_GlobalRainingAtBididibus.cpp

---
+++ uHunts/DONE_11786_GlobalRainingAtBididibus.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int getWater(const string& s){
+    int water = 0;
+    stack<int> downs;
+    
+    for(int i = 0; i < s.size(); i++){
+        if(s[i] == (char)92){
+            downs.push(i);
+        }
+        else if(s[i] == '/' && !downs.empty()){
+            int j = downs.top(); downs.pop();
+            water += i - j; // we get the water inside those chars
+        }
+    }
+    
+    return water;
+}
+
+int main(){
+    int T; cin >> T;
+    
+    for(int t = 0; t < T; t++){
+        string s; cin >> s;
+        int water = getWater(s);
+        cout << water << endl;
+    }
+    
+    return 0;
+}
